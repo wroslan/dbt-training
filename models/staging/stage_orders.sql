76d7f085354e404bdf7d8a6e9a4b2ec84422f4d1
wafamanii56@gmail.com <wafamanii56@gmail.com>
2026-05-22 07:50:35 +0000

    developing stage_orders

---
+++ models/staging/stage_orders.sql
@@ -0,0 +1,22 @@
+select 
+--from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit, 
+o.ordercostprice,
+o.ordersellingprice,
+--from raw customer
+c.customername, 
+c.segment,
+c.country,
+-- from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on o.customerId = c.customerId
+left join {{ ref('raw_products') }} as p
+on o.productId = p.productId
