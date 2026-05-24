29dc7ed1c3995099e3c6daf34cdff220b461ff64
Wafa Roslan <wafamanii56@gmail.com>
2026-05-24 14:05:04 +0000

    added macros

--- models/staging/stage_orders.sql
+++ models/staging/stage_orders.sql
@@ -16,7 +16,8 @@ c.country,
 p.productId,
 p.category,
 p.productname,
-p.subcategory
+p.subcategory,
+{{ markup('ordersellingprice','ordercostprice') }} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customers') }} as c
 on o.customerId = c.customerId
